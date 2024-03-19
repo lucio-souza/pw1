bc7c3d57bc5a72e8d082e79b1a71fe3b31f5321e
lucio-souza <desouzaleitelucioweslley@gmail.com>
2024-03-18 21:53:12 -0300

    terminando crud de transações

--- src/server.ts
+++ src/server.ts
@@ -81,10 +81,9 @@ server.get('/transactions',(req,res)=>{
     res.status(201).json(users)
 })
 
-server.post('/transactios',(req,res)=>{
+server.post('/transactions',(req,res)=>{
     const {value,type}=req.body as bodytransactions;
     const {id}=req.headers; 
-
     const cliente=users.find(client=>client.id===id);
 
     if(!cliente){
@@ -97,32 +96,44 @@ server.post('/transactios',(req,res)=>{
         value,
         date:new Date()
     }
+    if(transactions.type==="credit" || transactions.type==="debit"){
     cliente.transactions.push(transactions)
     res.status(201).json("usuario cadatrado")
+    }else{
+        res.send("tipo de transação invalida")
+    }
 })
 
-server.put('/transactions/:id',(req,res)=>{
-    const {id}=req.params;
-    const {cpf,name}=req.body
+server.put('/transactions/:idUser', (req, res) => {
+    const { id } = req.headers;
+    const {idUser}=req.params
+    const { value} = req.body;
 
-    const indice=users.findIndex(user=>user.id===id);
-    if(indice<0){
-        return res.status(404).json("erro ao atualizar")
-    }
-    users[indice]={
-        id,
-        cpf,
-        name,
-        transactions:users[indice].transactions
+    const indexUser = users.findIndex(user => user.id === idUser);
+    if(indexUser===-1){
+        res.send(`${idUser} ${id}`)
     }
 
-})
+    const transactionIndex = users[indexUser].transactions.findIndex(trans => trans.id === id);
+    if (transactionIndex === -1) {
+        return res.status(404).json("Transação não encontrada.");
+    }
 
-server.delete('/transactions/:id',(req,res)=>{
-    const {id}=req.params;
-    const indice=users.findIndex(users=>users.id===id);
-    if(indice<0) return res.status(404).json("erro ao deletar")
-    users.splice(indice,1)
+    // Atualizando os dados da transação
+    users[indexUser].transactions[transactionIndex].value = value;
+    res.status(200).json("Transação atualizada com sucesso.");
+});
+
+
+
+server.delete('/transactions/:idUser',(req,res)=>{
+    const {id}=req.headers
+    const {idUser}=req.params;
+
+    const indexUser=users.findIndex(users=>users.id===idUser);
+    const transactionIndex = users[indexUser].transactions.findIndex(trans => trans.id === id);
+
+    users[indexUser].transactions.splice(transactionIndex,1)
     res.status(200).json("usuario deletado")
 })
 
